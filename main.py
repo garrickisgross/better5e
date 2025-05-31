9ad71632f70b77f8c6024d06d471ef518091b650
= <=>
2025-05-31 14:42:09 -0500

    sizing change

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from screens.main_screen.main_screen import MainScreen
 def main():
     app = QtWidgets.QApplication(sys.argv)
     widget = MainScreen()
+    widget.setGeometry(100,100, 1080, 720)
     widget.show()
     sys.exit(app.exec())
    
