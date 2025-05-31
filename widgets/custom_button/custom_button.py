f2dda958eef4d5314643f52a17a01b6a8976afba
= <=>
2025-05-31 14:40:38 -0500

    cleanup: Fix custom button widget

--- widgets/custom_button/custom_button.py
+++ widgets/custom_button/custom_button.py
@@ -9,11 +9,3 @@ class custom_button(QtWidgets.QPushButton):
         self.setCursor(QtCore.Qt.PointingHandCursor)
         self.clicked.connect(on_click)
         self.setMinimumHeight(60)
-
-    def enterEvent(self, event):
-        self.setStyleSheet("background-color: deepskyblue; font-size: 16px; padding: 10px;")
-        super().enterEvent(event)
-
-    def leaveEvent(self, event):
-        self.setStyleSheet("font-size: 16px; padding: 10px;")
-        super().leaveEvent(event)
